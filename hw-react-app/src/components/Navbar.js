18c36225db5d6d049edad87006f1251e9c1cf570
jrizo19 <89281518+jrizo19@users.noreply.github.com>
2023-12-05 20:11:54 -0600

    fixed navbar formatting

--- hw-react-app/src/components/Navbar.js
+++ hw-react-app/src/components/Navbar.js
@@ -6,20 +6,24 @@ import logo from '../harry.jpeg';
 function Navbar() {
     return (
         <div>
-        <Nav className="justify-content-start mainNav" activeKey="/">
-            <img  id={"harryLogo"} src={logo} alt={"Harry Logo"}/>
-            <Nav.Item>
-                <Nav.Link as={NavLink} to='/'><span className="navItem">Home</span></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={NavLink} to='/customers'><span className="navItem">Customers</span></Nav.Link>
-            </Nav.Item>
-            <Nav.Item className="navItem">
-                <Nav.Link as={NavLink} to='/products'><span className="navItem">Products</span></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={NavLink} to='/sales'><span className="navItem">Sales</span></Nav.Link>
-            </Nav.Item>
+        <Nav className="mainNav" activeKey="/">
+            <div>
+                <img  id={"harryLogo"} src={logo} alt={"Harry Logo"}/>
+            </div>
+            <div id={"allNavs"}>
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to='/'><span className="navItem">Home</span></Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to='/customers'><span className="navItem">Customers</span></Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="navItem">
+                    <Nav.Link as={NavLink} to='/products'><span className="navItem">Products</span></Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to='/sales'><span className="navItem">Sales</span></Nav.Link>
+                </Nav.Item>
+            </div>
         </Nav>
         </div>
     );
